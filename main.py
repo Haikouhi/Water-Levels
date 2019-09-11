f76363fc0d13f7db05f419281b68528c4675949b
Haikouhi <h.oroudjian@protonmail.com>
2019-09-11 13:54:43 +0200

    gitignore

---
+++ main.py
@@ -0,0 +1,100 @@
+Jason is a family of three satellite altimetry satellites jointly developed by NASA and CNES to study the ocean circulation and the interactions between the oceans and the atmosphere. The purpose of these missions is to improve climate change forecasts and to monitor oceanic phenomena such as El Niño or oceanic swirl zones. To fulfill these objectives, these spacecraft carry equipment for measuring sea level with a very high accuracy. These satellites, which took over from TOPEX / Poseidon, were placed in orbit respectively in 2001 (Jason-1). , 2008 (Jason-2) and 2016 (Jason-3).
+
+Apprenez à prononcer
+Ouvrir dans Google Traduction	
+Commentaires
+Résultat Web avec des liens annexes
+Google Traduction
+
+https://translate.google.com › ...
+Ce service gratuit de Google traduit instantanément des mots, des expressions et des pages Web du français vers plus de 100 autres langues.
+Google Translate Google
+Google's free service instantly translates words, phrases, and ...
+Autres résultats sur google.com »
+Résultats Web
+translate - English-French Dictionary WordReference.com
+
+https://www.wordreference.com › enfr › translate
+translate - traduction anglais-français. Forums pour discuter de translate, voir ses formes composées, des exemples et poser vos questions. Gratuit.
+translate - Traduction française – Linguee
+
+https://www.linguee.fr › anglais-francais › traduction › translate
+De très nombreux exemples de phrases traduites contenant "translate" – Dictionnaire français-anglais et moteur de recherche de traductions françaises.
+translate - Traduction en français - exemples anglais ...
+
+https://context.reverso.net › traduction › anglais-francais › translate
+Traductions en contexte de "translate" en anglais-français avec Reverso Context : translate into, need to translate, translate to, able to translate, in order to ...
+Amazon Translate – Traduction automatique neuronale – AWS
+
+https://aws.amazon.com › translate
+Amazon Translate est un service de traduction automatique neuronale offrant des traductions linguistiques rapides, abordables et d'excellente qualité.
+Google Translate - Google Traduction
+
+https://translate.google.fr › ...
+Ce service gratuit de Google traduit instantanément des mots, des expressions et des pages Web du français vers plus de 100 autres langues.
+SYSTRAN Translate
+
+https://translate.systran.net › translationTools › text
+Use SYSTRAN Translate, the 1st free online translator providing specialized neural translation models trained by a worldwide expert community.
+Quand Google Translate pète un câble... - Actualités SEO et ...
+
+https://www.abondance.com › 20180724-32648-quand-google-translate-p...
+Note : 5 - ‎2 votes
+24 juil. 2018 - 24 juillet 2018 - Des internautes ont découvert que lorsqu'on demandait à Google Translate certaines traductions depuis des langues peu ...
+translate between - French translation – Linguee
+
+https://www.linguee.com › english-french › translation › translate+between
+Many translated example sentences containing "translate between" – French-English dictionary and search engine for French translations.
+Autres questions posées
+Comment traduire un site allemand en français ?
+
+
+Comment se servir de Google Traduction ?
+
+
+Comment mettre la traduction automatique ?
+
+
+Comment traduire un document sur Google ?
+
+
+Commentaires
+Recherches associées à translate
+
+translate reverso
+
+linguee translate
+
+traduire
+
+word translate
+
+translate wordreference
+
+world translate
+
+amazon translate
+
+traduction de translate
+Navigation par pages
+	1	
+2
+	
+3
+	
+4
+	
+5
+	
+6
+	
+7
+	
+8
+	
+9
+	
+10
+	
+Suivant
+Liens de pied de page
